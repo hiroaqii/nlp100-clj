b301a8111ea3131549f34b33d6cf4a676c2c7c2a
hiroaqii <hiroaqii@gmail.com>
2018-01-24 00:21:34 +0900

    Add p13, p14, p15

--- src/nlp100_clj/chap02.clj
+++ src/nlp100_clj/chap02.clj
@@ -35,3 +35,47 @@
                   (map #(str (nth % (dec n)) "\n")))]
      (spit (format "out/col%d.txt" n) (str/join col)))))
 
+
+(defn p13
+  "13. col1.txtとcol2.txtをマージ
+
+  12で作ったcol1.txtとcol2.txtを結合し，
+  元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．
+  確認にはpasteコマンドを用いよ．"
+  ([] (p13 "out/col1.txt" "out/col2.txt" "out/p13_out.tsv"))
+  ([in1 in2 out]
+   (let [col1 (str/split-lines (slurp in1))
+         col2 (str/split-lines (slurp in2))]
+     (->> (map #(str %1 "\t" %2 "\n") col1 col2)
+          (str/join)
+          (spit out)))))
+
+
+(defn p14
+  "14. 先頭からN行を出力
+
+  自然数Nをコマンドライン引数などの手段で受け取り，
+  入力のうち先頭のN行だけを表示せよ．
+  確認にはheadコマンドを用いよ．"
+  ([] (p14 file-path 10))
+  ([n] (p14 file-path n))
+  ([file n]
+   (let [col (take n (str/split-lines (slurp file)))
+         s (str/join (map #(str % "\n") col))]
+     (print s))))
+
+
+(defn p15
+  "15. 末尾のN行を出力
+
+  自然数Nをコマンドライン引数などの手段で受け取り，
+  入力のうち末尾のN行だけを表示せよ．
+  確認にはtailコマンドを用いよ．"
+  ([] (p15 file-path 10))
+  ([n] (p15 file-path n))
+  ([file n]
+   (let [col (take-last n (str/split-lines (slurp file)))
+         s (str/join (map #(str % "\n") col))]
+     (print s))))
+
+
