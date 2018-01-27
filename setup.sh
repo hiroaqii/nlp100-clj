16f1807a1d179c7b2d19e6412d1de2270196df95
hiroaqii <hiroaqii@gmail.com>
2018-01-27 21:26:58 +0900

    Add

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+RESOURCE_DIR=$(cd $(dirname $0)/resources && pwd)
+echo $RESOURCE_DIR
+
+# chap03
+wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/jawiki-country.json.gz -P ${RESOURCE_DIR} 
+gzip -d ${RESOURCE_DIR}/jawiki-country.json.gz
