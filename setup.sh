89016eab063075351dd0ad4d54be726750aa6dba
hiroaqii <hiroaqii@gmail.com>
2018-02-06 00:44:58 +0900

    Set up chpa04

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ echo $RESOURCE_DIR
 # chap03
 wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/jawiki-country.json.gz -P ${RESOURCE_DIR} 
 gzip -d ${RESOURCE_DIR}/jawiki-country.json.gz
+
+#chap04
+wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt -P ${RESOURCE_DIR} 
