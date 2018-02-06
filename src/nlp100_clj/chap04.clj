f876a6a6a2620326b662574ba44637252eb7c4a6
hiroaqii <hiroaqii@gmail.com>
2018-02-07 00:08:30 +0900

    Add

---
+++ src/nlp100_clj/chap04.clj
@@ -0,0 +1,28 @@
+(ns nlp100-clj.chap04
+  "第4章: 形態素解析
+
+  メモ：MeCabではなくkuromojiを使用。
+  "
+  (:require [clojure.string :as str])
+  (:import [com.atilika.kuromoji.ipadic Token Tokenizer]))
+
+
+(def tokenizer (Tokenizer.))
+
+
+(defn tokenize [s]
+  (let [col (.tokenize tokenizer s)]
+    (map #(apply str (.getSurface %1) "\t" (.getAllFeatures %1)) col)))
+
+
+(defn init
+  "夏目漱石の小説『吾輩は猫である』の文章（neko.txt）をkuromojiを使って形態素解析し，
+  その結果をneko.txt.kuromojiというファイルに保存
+  (オリジナルの問題ははKuromojiでなくMeCabを使用する）"
+
+  []
+  (let [tokens (flatten (map #(tokenize %) (str/split-lines (slurp "resources/neko.txt"))))
+        s (str/join "\n" tokens)]
+    (spit "resources/neko.txt.kuromoji" s)))
+
+;(init)
