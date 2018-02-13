6d70fbb2571c6e7ac415ddcc4e36991880101023
hiroaqii <hiroaqii@gmail.com>
2018-02-14 00:20:04 +0900

    Add p36

--- src/nlp100_clj/chap04.clj
+++ src/nlp100_clj/chap04.clj
@@ -100,3 +100,13 @@
        (map #(apply str %))))
 
 
+(defn p36
+  "36. 単語の出現頻度
+
+  文章中に出現する単語とその出現頻度を求め，出現頻度の高い順に並べよ"
+  []
+  (->> (map :surface (p30))
+       (frequencies)
+       (sort-by val >)))
+
+
