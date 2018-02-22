d79bf3088fe696d0a39b16669e850a452fc642b5
hiroaqii <hiroaqii@gmail.com>
2018-02-23 00:12:23 +0900

    Add incanter

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.atilika.kuromoji/kuromoji-ipadic "0.9.0"]
+                 [incanter/incanter-core "1.5.7"]
+                 [incanter/incanter-charts "1.5.7"]
                  [cheshire "5.8.0"]
                  [clj-http "3.7.0"]])
